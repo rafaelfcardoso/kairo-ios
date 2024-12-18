28fe05a47a55fc3dbcb4b7a1faa8ecaa49e2fe5f
rafaelfernandescardoso <rafael.fcardoso@hotmail.com>
2024-12-18 10:15:59 -0300

    Initial Commit

---
+++ ProjectZenith/ProjectZenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectZenithApp.swift
+//  ProjectZenith
+//
+//  Created by Rafael Cardoso on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectZenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectZenithTests/ProjectZenithTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectZenithTests.swift
+//  ProjectZenithTests
+//
+//  Created by Rafael Cardoso on 18/12/24.
+//
+
+import Testing
+
+struct ProjectZenithTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
